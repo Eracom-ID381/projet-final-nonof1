93f8decf60648f94d9d96c9c0f990977b2947bf4
nonof1 <60931115+nonof1@users.noreply.github.com>
2020-06-09 15:25:36 +0200

    ml5 poseNet model added

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,23 @@
+let video;
+let poseNet;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+
 }
 
-function draw() { 
+function modelLoaded() {
+    console.log('poseNet model is ready!')
+}
 
+function draw() {
+    image(video, 0, 0);
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(windowWidth, windowHeight);
 }
