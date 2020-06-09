456c7ff282fe5f7012f1419c7b340709a8b633ed
nonof1 <60931115+nonof1@users.noreply.github.com>
2020-06-09 15:59:48 +0200

    Model fully working
    - Tested it with an ellipse on the right eye

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,6 @@
 let video;
 let poseNet;
+let pose;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -8,14 +9,26 @@ function setup() {
 
     poseNet = ml5.poseNet(video, modelLoaded);
 
+    poseNet.on('pose', poseDetected);
 }
 
 function modelLoaded() {
-    console.log('poseNet model is ready!')
+    console.log('poseNet model is ready!');
+}
+
+function poseDetected(poses) {
+    console.log('poses!');
+    if (poses.length > 0) {
+        pose = poses[0].pose;
+    }
 }
 
 function draw() {
     image(video, 0, 0);
+    if (pose) {
+        fill(0, 255, 0);
+        ellipse(pose.rightEye.x, pose.rightEye.y, 30);
+    }
 }
 
 function windowResized() {
